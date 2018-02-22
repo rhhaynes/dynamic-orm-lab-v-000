02a9f56feacf3600f4270f57d0e2ba5d3ab963ba
Randall Harris Haynes <harrishaynes@gmail.com>
2018-02-22 18:51:02 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -19,7 +19,6 @@ class InteractiveRecord
   end
   
   def self.find_by(attributes)
-    binding.pry
     sql = "SELECT * FROM #{table_name} WHERE #{attributes.keys[0]} = ?"
     DB[:conn].execute(sql,attributes.values[0]).flatten
   end
